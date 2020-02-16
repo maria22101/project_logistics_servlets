7ff47d54235fd2f1cc147613e84585f2a73dfd71
Maria Bilous <maria.bilous101@gmail.com>
2020-02-16 22:11:29 +0200

    05_registration: form validation implemented

---
+++ src/main/java/ua/train/project_logistics_servlets/constant/RegexConstant.java
@@ -0,0 +1,8 @@
+package ua.train.project_logistics_servlets.constant;
+
+public interface RegexContainer {
+    String REGEX_NAME = "regex.name";
+    String REGEX_SURNAME = "regex.surname";
+    String REGEX_EMAIL = "regex.email";
+    String REGEX_PASSWORD = "regex.password";
+}
---
+++ src/main/java/ua/train/project_logistics_servlets/service/RegistrationValidationService.java
@@ -0,0 +1,4 @@
+package ua.train.project_logistics_servlets.service;
+
+public class RegistrationValidationService {
+}
